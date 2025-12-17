638b3e1577e415aa926f22c9173bdc54afa0baee
Harikapadala1234 <147389821+Harikapadala1234@users.noreply.github.com>
2025-12-17 11:18:01 +0530

    Add CanteenDetails class to display menu items

---
+++ CanteenDetails.java
@@ -0,0 +1,6 @@
+public class CateenDetails{
+public static void main(String args[]){
+System.out.println("---welcome to College Canteen---");
+System.out.println("Tea- "+args[0]+"\n"+"Coffee- "+args[1]+"\n"+"Sandwich- "+args[2]+"\n"+"Burger- "+args[3]+"\n"+"Juice- "+args[4]+"\n"+"Briyani- "+args[5]+"\n"+"Veg Fried rice- "+args[6]+"\n"+"Egg Fried Rice- "+args[7]+"\n"+"Veg Noodles- "+args[8]+"\n"+"Egg Noodles- "+args[9]);
+}
+}
