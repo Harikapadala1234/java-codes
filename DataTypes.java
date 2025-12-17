b2d5d1edeaa7c62c5423b8f049f9d5d760cee12a
Harikapadala1234 <147389821+Harikapadala1234@users.noreply.github.com>
2025-12-17 11:47:19 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes{
+   public static void main(String args[])
+     {
+                int a=10;
+		short s=12456;
+		long l=12345678;
+		double d=12.456;
+		float f=123.56f;
+		char ch='A';
+		byte b=123;
+		boolean bool=true;
+		System.out.println("Int value:"+a);
+		System.out.println("Short value:"+s);
+		System.out.println("Long value:"+l);
+		System.out.println("Double Value:"+d);
+		System.out.println("Float Value:"+f);
+		System.out.println("Char value:"+ch);
+		System.out.println("Byte Value:"+b);
+		System.out.println("Boolean Value:"+bool);
+     }
+}
---
+++ DefaultValues.java
@@ -0,0 +1,23 @@
+public class DefaultValues{
+     
+         static int a;
+	 static short s;
+	 static long l;
+	 static double d;
+	 static float f;
+	 static char ch;
+	 static byte b;
+	 static boolean bool;
+
+   public static void main(String args[])
+     {
+		System.out.println("Int value:"+a);
+		System.out.println("Short value:"+s);
+		System.out.println("Long value:"+l);
+		System.out.println("Double Value:"+d);
+		System.out.println("Float Value:"+f);
+		System.out.println("Char value:"+ch);
+		System.out.println("Byte Value:"+b);
+		System.out.println("Boolean Value:"+bool);
+     }
+}
