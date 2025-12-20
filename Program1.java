fba27e32c74ccc223a5f3ed6a16a1011ba8a900c
Harikapadala1234 <147389821+Harikapadala1234@users.noreply.github.com>
2025-12-20 13:07:06 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class Program1 {
+   public static void main(String[] args) {
+	System.out.println("Hello Everyone");
+	System.out.println(args[0]);
+	System.out.println(args[1]);
+	System.out.println(args[2]);
+	System.out.println(args[3]);
+	System.out.println(args[4]);
+	System.out.println(args[5]);
+	System.out.println(args[6]);
+	System.out.println(args[7]);
+	System.out.println("Array Values:"+args[0]+" "+args[1]+" "+args[2]+" "+args[3]+" "+args[4]+" "+args[5]+" "+args[6]+" "+args[7]);
+}
+}
+  
