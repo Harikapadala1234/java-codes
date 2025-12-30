df17aa83aa0da2bc0b892c0a561271dc687bed7f
Harikapadala1234 <147389821+Harikapadala1234@users.noreply.github.com>
2025-12-30 15:46:47 +0530

    Add Bitwise operations example in Bitwise.java

---
+++ Bitwise.java
@@ -0,0 +1,15 @@
+
+public class Bitwise {
+
+	public static void main(String[] args) {
+		int a=16;
+		int b=8;
+		System.out.println(a&b);
+		System.out.println(a|b);
+		System.out.println(a^b);
+		System.out.println(~a);
+		System.out.println(a<<2);
+		System.out.println(a>>2);
+	}
+
+}
